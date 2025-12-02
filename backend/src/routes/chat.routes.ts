ea39afce71fef26a9b15369a31d82a8656f8157a
Rosa Palaci <61998089+Rosa-Palaci@users.noreply.github.com>
2025-12-01 18:42:24 -0600

    Revert "Connect chat API to ODA service"

--- backend/src/routes/chat.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { chatRecommend, chatSearch } from "../controllers/chat.controller";
-
-const router = Router();
-
-router.post("/chat/search", chatSearch);
-router.post("/chat/recommend", chatRecommend);
-
-export default router;
